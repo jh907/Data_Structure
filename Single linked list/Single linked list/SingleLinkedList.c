643f35e5279adfa8bbed744e4ec0b6d4cc81d96e
jh907 <jeje300011@naver.com>
2023-05-12 23:57:08 +0800

    Code correction

--- Single linked list/Single linked list/SingleLinkedList.c
+++ Single linked list/Single linked list/SingleLinkedList.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 typedef struct NODE {
-	
+
 	int nData;
 
 	struct NODE* next;
@@ -12,18 +12,20 @@ typedef struct NODE {
 int main() {
 
 	NODE list[5] = { 0 };
-
-	list[0].next = &list[1];
-	list[1].next = &list[2];
-	list[2].next = &list[3];
-	list[3].next = &list[4];
-	list[4].next = 0;
-
+	
+	//값 초기화
 	list[0].nData = 100;
 	list[1].nData = 200;
 	list[2].nData = 300;
 	list[3].nData = 400;
 	list[4].nData = 500;
+
+	//연결 리스트 구조화
+	list[0].next = &list[1];
+	list[1].next = &list[2];
+	list[2].next = &list[3];
+	list[3].next = &list[4];
+	list[4].next = 0;
 
 	for (int i = 0; i < 5; i++)
 		printf("list[%d] : %d\n", i, list[i].nData);
